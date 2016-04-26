565aa6fe4a319022f5b89a1d5d035d4ed1d39f62
Mugeri <nyotuo17@gmail.com>
2016-04-26 16:54:36 +0300

    super_sum function

---
+++ day_2/super_sum.py
@@ -0,0 +1,20 @@
+
+
+def super_sum(A):
+	'''
+	Takes a list A, and:
+		halves every even number
+		doubles every odd number
+	and returns the sum of all
+	'''
+	total = 0
+	
+	for each in A:
+		if each % 2== 0:
+			total += each / 2
+		else:
+			total += each * 2
+	
+	return total
+
+
